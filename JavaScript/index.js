e8e5ee871a0b3a9ed20fe57bed40d59ec6d9e288
Mathias Barbosa <mathibar03@gmail.com>
2023-07-11 21:42:18 -0300

    instalacion sass y introducccion a javaScript

---
+++ JavaScript/index.js
@@ -0,0 +1,21 @@
+alert('hola mundo')
+
+var nombreDePersona = 'Mathias' //string
+console.log(nombreDePersona)
+let apellido = "Barbosa"
+console.log(apellido);
+const EDAD = 23 ;
+console.log(EDAD);
+EDAD = 10
+console.log(EDAD);
+
+
+let email;
+email = 'matibar@algo.com'
+
+apellido = "barbosa velazquez";
+console.log(apellido);
+// EDAD = 10;
+
+
+//TODO tipos de datos y dif entre var y let 
